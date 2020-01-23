a25878cf70e9c1778b38fa852e44ed9007a8e301
Devin Gleason Lambert <dlambert@oattravel.com>
2020-01-23 14:17:07 -0500

    Adding GoogleDotComTests

--- GCTDevOpsInterviewTests/UriParserTests.cs
+++ GCTDevOpsInterviewTests/UriParserTests.cs
@@ -8,18 +8,66 @@ namespace GCTDevOpsInterviewTests
     public class UriParserTests
     {
         [TestMethod]
-        public void IsValid_GoogleDotCom()
+        public void GoogleDotCom_Valid_Scheme()
         {
             string googleDotCom = "https://www.google.com";
 
             string expectedScheme = "https";
+
+            Assert.AreEqual( expectedScheme, GCTDevOpsInterview.UriParser.GetScheme(googleDotCom) );
+        }
+
+        [TestMethod]
+        public void GoogleDotCom_Valid_Host()
+        {
+            string googleDotCom = "https://www.google.com";
+
             string expectedHost = "www.google.com";
+
+            Assert.AreEqual(expectedHost, GCTDevOpsInterview.UriParser.GetHost(googleDotCom));
+        }
+
+        [TestMethod]
+        public void GoogleDotCom_Valid_Path()
+        {
+            string googleDotCom = "https://www.google.com";
+
+            string expectedPath = "";
+
+            Assert.AreEqual(expectedPath, GCTDevOpsInterview.UriParser.GetPath(googleDotCom));
+        }
+
+        [TestMethod]
+        public void GoogleDotCom_Valid_Port()
+        {
+            string googleDotCom = "https://www.google.com";
+
             string expectedPort = "";
+
+            Assert.AreEqual(expectedPort, GCTDevOpsInterview.UriParser.GetPort(googleDotCom));
+        }
+
+        [TestMethod]
+        public void GoogleDotCom_Valid_Query()
+        {
+            string googleDotCom = "https://www.google.com";
+
             string expectedQuery = "";
+
+            Assert.AreEqual(expectedQuery, GCTDevOpsInterview.UriParser.GetQuery(googleDotCom));
+        }
+
+        [TestMethod]
+        public void GoogleDotCom_Valid_Fragment()
+        {
+            string googleDotCom = "https://www.google.com";
+
             string expectedFragment = "";
 
-            Assert.AreEqual(expectedScheme, GCTDevOpsInterview.UriParser.GetScheme(googleDotCom) ); 
+            Assert.AreEqual(expectedFragment, GCTDevOpsInterview.UriParser.GetFragment(googleDotCom));
         }
+
+
 
     }
 }
